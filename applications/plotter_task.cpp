605bdaafccdbb8946bcc6b406d3dcc8c7f5b718f
swanyu0316 <swanyu0316@gmail.com>
2025-10-10 19:45:41 +0800

    fix: fine

--- applications/plotter_task.cpp
+++ applications/plotter_task.cpp
@@ -5,10 +5,10 @@
 #include "motor/rm_motor/rm_motor.hpp"
 
 extern sp::DBus remote;
-extern sp::RM_Motor motor_6020_1;
-extern sp::RM_Motor motor_6020_2;
-extern sp::RM_Motor motor_6020_3;
-extern sp::RM_Motor motor_6020_4;
+extern sp::RM_Motor motor_3508_1;
+extern sp::RM_Motor motor_3508_2;
+extern sp::RM_Motor motor_3508_3;
+extern sp::RM_Motor motor_3508_4;
 
 sp::Plotter plotter(&huart1);
 
@@ -18,13 +18,13 @@ extern "C" void plotter_task()
 
   while (true) {
 #if TEST_MOTOR_ID == 1
-    plotter.plot(remote.ch_lh, motor_6020_1.speed);
+    plotter.plot(remote.ch_lh, motor_3508_1.speed);
 #elif TEST_MOTOR_ID == 2
-    plotter.plot(remote.ch_lh, motor_6020_2.speed);
+    plotter.plot(remote.ch_lh, motor_3508_2.speed);
 #elif TEST_MOTOR_ID == 3
-    plotter.plot(remote.ch_lh, motor_6020_3.speed);
+    plotter.plot(remote.ch_lh, motor_3508_3.speed);
 #elif TEST_MOTOR_ID == 4
-    plotter.plot(remote.ch_lh, motor_6020_4.speed);
+    plotter.plot(remote.ch_lh, motor_3508_4.speed);
 #endif
     osDelay(1);
   }
